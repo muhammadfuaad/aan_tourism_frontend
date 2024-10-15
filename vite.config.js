1204f86cec86904d14b2e51fe38906405d19d2df
muhammadfuaad <fuaad29@gmail.com>
2024-10-15 18:24:39 +0500

    code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aan_tourism_frontend/',
+  // base: '/aan_tourism_frontend/',
   build: {
     outDir: 'build',  // Ensure the output directory is 'build'
   },
